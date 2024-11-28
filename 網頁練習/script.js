718284f0163b7069b41b8147be0372ec634e6f41
Valerie0613 <162416229+Valerie0613@users.noreply.github.com>
2024-11-28 17:51:43 +0800

    Add files via upload

---
+++ 網頁練習/script.js
@@ -0,0 +1,16 @@
+/* scripts.js */
+document.getElementById('boxA').addEventListener('click', function() {
+    window.location.href = 'pageA.html';
+});
+
+document.getElementById('boxB').addEventListener('click', function() {
+    window.location.href = 'pageB.html';
+});
+
+document.getElementById('boxC').addEventListener('click', function() {
+    window.location.href = 'pageC.html';
+});
+
+document.getElementById('boxD').addEventListener('click', function() {
+    window.location.href = 'pageD.html';
+});
